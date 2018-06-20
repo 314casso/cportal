ad0021de42ebd119a9f7f6a66f4d8e4bd11ec827
picasso <picasso75@yandex.ru>
2018-06-20 14:15:07 +0300

    fix

--- export/contpics/services.py
+++ export/contpics/services.py
@@ -16,12 +16,12 @@ class ContpicsService(SudsService):
                 status=nutep.models.DateQueryEvent.PENDING, company=company)                                             
             if company.ukt_guid:         
                 response = self._client.service.getClientPicturesZip(company.ukt_guid, start_date, end_date)                                   
-                if response:                                        
-                    file_data = base64.b64decode(response.data)                                                                               
-                    filename = u'%s-%s-%s.%s' %  (company, start_date, end_date, 'zip')
-                    file_store = event.files.create(title=filename)             
-                    file_store.file.save(filename, ContentFile(file_data))
-                    event.status = nutep.models.DateQueryEvent.SUCCESS                                                           
+                if response:
+                    for f in response.pictures:
+                        file_data = base64.b64decode(f.data)
+                        filename = u'%s-%s-%s.%s' %  (company, start_date, end_date, 'zip')
+                        file_store = event.files.create(title=filename)             
+                        file_store.file.save(filename, ContentFile(file_data))                    
             event.status = nutep.models.DateQueryEvent.SUCCESS
             event.save()
         except Exception, e:
