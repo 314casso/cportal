3c59c88efe2974abf20997815f470fe3a8c1ca88
Picasso <it-support2@ruscon.global>
2021-10-05 17:03:33 +0300

    fix

--- export/contpics/tasks.py
+++ export/contpics/tasks.py
@@ -4,9 +4,10 @@ from contpics.services import ContpicsService
 from export.local_settings import WEB_SERVISES
 from export.settings import BASE_RQ_PROC
 
+SERVICE_NAME = 'cp_dc'
 
 @job(BASE_RQ_PROC)
 def contpics_task(user, company, start_date, end_date):            
-    service = ContpicsService(WEB_SERVISES['contpics'])    
+    service = ContpicsService(WEB_SERVISES[SERVICE_NAME])    
     service.run(user, company, start_date, end_date)      
                                       
